41c22c66c9634f373fd7ac3cb857c327d9aadc40
Luana <ls.luanastephany@gmail.com>
2021-05-30 17:36:14 -0300

    criando variaveis dos dados

--- src/Repository.js
+++ src/Repository.js
@@ -1,12 +1,27 @@
+import { FiStar } from 'react-icons/fi'
+
+const data = {
+  title: 'meu-primeiro-codigo',
+  description: 'Descrevendo meu código',
+  stars: 200,
+  updated: 'Updated May 29'
+}
+
 function Repository() {
   return (
     <div className="repository">
-      <h4 className="title">
-        meu-primeiro-codigo
-      </h4>
-      <span className="description">
-        Descrevendo meu código
-      </span>
+      <h4 className="title">{data.title}</h4>
+
+      <span className="description">{data.description}</span>
+
+      <div className="info">
+        <div className="stars">
+          <FiStar size={16} />
+          <span>{data.stars}</span>
+        </div>
+        <span className="updated">{data.updated}</span>
+      </div>
+
     </div>
   )
 }
