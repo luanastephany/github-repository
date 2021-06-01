d0133294a05737a8166a6bcd4526dfbc9d6d047a
Luana <ls.luanastephany@gmail.com>
2021-06-01 16:02:33 -0300

    finalizando projeto

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import { FaGithub } from 'react-icons/fa'
 import Repository from './Repository'
+import axios from 'axios'
+import { useState } from 'react'
 
-const items = [0, 0, 0, 0]
 
 function App() {
+  const [user, setUser] = useState('')
+
+  const [repositories, setRepositories] = useState([])
+
+  const search = async () => {
+    const url = `https://api.github.com/users/${user}/repos`
+    const result = await axios.get(url)
+    setRepositories(result.data)
+  }
+
+  const image = repositories?.[0]?.owner?.avatar_url
+
   return (
     <div className="app">
       <header>
         <FaGithub color="white" size={32} />
-        <input type="text" placeholder="Digite o nome do usuário" />
-        <button>Search</button>
+        <input
+          type="text"
+          placeholder="Digite o nome do usuário"
+          value={user}
+          onChange={event => setUser(event.target.value)}
+        />
+
+        <button onClick={search}>Search</button>
       </header>
 
       <div className="repositories">
-        <Repository />
-        <Repository />
-        <Repository />
-        <Repository />
+        {image && <img src={image} alt="user" />}
+        {repositories.map(item => (
+          <Repository data={item} />
+        ))}
       </div>
 
     </div>
